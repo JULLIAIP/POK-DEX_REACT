3dd2b598edba0925b8faee4aff00f4a01826e64d
JULLIAIP <contato.julliaizidorio@gmail.com>
2022-05-06 18:07:51 -0300

    estrutura da pagina de pokemons e componente card

--- pokedex/src/pages/PreviewPokemonsPage/index.js
+++ pokedex/src/pages/PreviewPokemonsPage/index.js
@@ -1,6 +1,8 @@
 import Header from "../../components/header";
 import HomeIcon from "@mui/icons-material/Home";
 import { NavLink } from "react-router-dom";
+import { Grid } from "@mui/material";
+import ActionAreaCard from "../../components/card";
 
 export default function PreviewPage() {
   return (
@@ -10,7 +12,21 @@ export default function PreviewPage() {
         to="/"
         icon={<HomeIcon variant="contained" color="secondary" />}
       />
-      <NavLink to="/details">Ver Detalhes</NavLink>
+    
+      <Grid container spacing={2}>
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <ActionAreaCard />
+        </Grid>
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <ActionAreaCard />
+        </Grid>
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <ActionAreaCard />
+        </Grid>
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <ActionAreaCard />
+        </Grid>
+      </Grid>
     </div>
   );
 }
